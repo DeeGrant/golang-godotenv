03d84493c60bf9b31a3ff12e0e0726d61a0d1f35
Dee Grant <WalterGrant@gmail.com>
2022-06-27 17:34:47 -0500

    example

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # golang-godotenv
+
+A simple example of using [GoDotEev](https://github.com/joho/godotenv) to set up environmental variables.
+
+`go run .`
+
+[cite video](https://www.youtube.com/watch?v=mnCgl-iwPak)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+func main() {
+	fmt.Println("")
+
+	// Load environment variables
+	err := godotenv.Load(".env")
+	if err != nil {
+		fmt.Printf("Could not load .env file")
+		os.Exit(1)
+	}
+
+	// Check environment variable
+	token, ok := os.LookupEnv("APP_TOKEN")
+	if !ok {
+		fmt.Printf("Token not found")
+	}
+	fmt.Println("App Token: ", token)
+
+	// Get environment variable
+	fmt.Println("DB String: ", os.Getenv("DB_STRING"))
+
+	fmt.Println("")
+
+	// Set an environment variable
+	err = os.Setenv("NEW_ENV_VAR", "SomethingNew")
+	fmt.Println("New War: ", os.Getenv("NEW_ENV_VAR"))
+	fmt.Println("")
+
+	// Lead environment variables into a map
+	envMap, err := godotenv.Read(".env")
+	if err != nil {
+		fmt.Printf("Could not load .env file into map[string]string\n")
+		os.Exit(1)
+	}
+
+	fmt.Println("App Token: ", envMap["APP_TOKEN"])
+	fmt.Println("Token:     ", envMap["DB_STRING"])
+
+}
